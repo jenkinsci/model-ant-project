afc0cea53952084f28eee532896f0af94357233b
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-02-22 11:57:02 -0800

    making the coverage look bit more real

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
+        new Hello().foo();
+    }
+    public void foo() {
         System.out.println("Hello, world!");
     }
 }
--- test/HelloTest.java
+++ test/HelloTest.java
@@ -3,4 +3,7 @@ import junit.framework.TestCase;
 public class HelloTest extends TestCase {
     public void test1() {}
     public void test2() {}
+    public void test3() {
+        new Hello().foo();
+    }
 }
